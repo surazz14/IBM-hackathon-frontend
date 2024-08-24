a8bf06a19667e9fa79f1cf89d782ffd012ae2f3d
suraj <suraj@listoglobal.com>
2024-08-24 14:19:54 +0545

    fix: remove mobx

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "mobx-react";
 import { ThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import theme from "./theme";
-import store from "./entities/setUpStore";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
@@ -19,11 +17,9 @@ function WrapThemeProvider(props) {
 }
 
 ReactDOM.render(
-  <Provider {...store}>
     <WrapThemeProvider>
       <App />
-    </WrapThemeProvider>
-  </Provider>,
+    </WrapThemeProvider>,
   document.getElementById("root")
 );
 
--- src/route/route.js
+++ src/route/route.js
@@ -1,47 +1,28 @@
 import React from "react";
 import { Route, Switch, Router as BrowserRouter } from "react-router-dom";
-import { inject, observer } from "mobx-react";
 import Box from "@material-ui/core/Box";
 
 import routes from "./";
 import apphistory from "apphistory";
 
-function RouterSwitch({ token }) {
-  if (token) {
-    apphistory.push("/review");
-  }
+function RouterSwitch() {
   return (
     <Switch>
       {routes.map(({ ...rest }, index) => {
-        if (token) {
-          if (rest.is_private) {
-            return <Route key={index} {...rest} />;
-          }
-        } else {
-          if (!rest.is_private) {
-            return <Route key={index} {...rest} />;
-          }
-        }
-        return null;
+          return <Route key={index} {...rest} />;
       })}
     </Switch>
   );
 }
 
-const Router = ({ user }) => {
-  const { token } = user;
+const Router = () => {
   return (
     <BrowserRouter history={apphistory}>
-      {token && (
-        <Box>
-        </Box>
-      )}
-
-      <Box ml={token ? "200px" : 0}>
-        <RouterSwitch token={token} />
+      <Box>
+        <RouterSwitch />
       </Box>
     </BrowserRouter>
   );
 };
 
-export default inject("user")(observer(Router));
+export default Router
